88b17c5fb9a50f4ae793b86addef3b651681fccd
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-01-06 21:54:48 +0530

    Adding a folder for different games

---
+++ Tic_Tac_Toe/script.js
@@ -0,0 +1,69 @@
+const boxes = document.querySelectorAll(".box");
+const statusText = document.getElementById("status");
+const overlay = document.querySelector(".overlay");
+const resultText = document.getElementById("result-text");
+const restartBtn = document.getElementById("restart")
+const clickSound = new Audio("sounds/click.wav");
+const winSound = new Audio("sounds/win.wav");
+let turnX = true;
+let gameOver = false;
+
+const winPatterns = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
+];
+
+boxes.forEach(box => {
+  box.addEventListener("click", () => {
+    if (box.innerText !== "" || gameOver) return;
+    clickSound.currentTime = 0;
+    clickSound.play();
+    if(turnX){
+      box.innerText = "X";
+      box.classList.add("x");
+    }else {
+      box.innerText = "O";
+      box.classList.add("o");
+    }
+    checkWinner();
+    if(!gameOver){
+        turnX = !turnX;
+        statusText.innerText = turnX ? "Player X turn" : "Player O turn";
+    }
+  });
+});
+
+function checkWinner() {
+  for (let pattern of winPatterns) {
+    const [a, b, c] = pattern;
+
+    if (
+      boxes[a].innerText &&
+      boxes[a].innerText === boxes[b].innerText &&
+      boxes[a].innerText === boxes[c].innerText
+    ) {
+      winSound.currentTime = 0;
+      winSound.play();
+      showResult(`Winner is ${boxes[a].innerText}`);
+      gameOver = true;
+      return;
+    }
+  }
+
+  const isDraw = [...boxes].every(box => box.innerText !== "");
+  if (isDraw) {
+    showResult("It's a Draw");
+    gameOver = true;
+  }
+}
+function showResult(message){
+  resultText.innerText = message;
+  overlay.classList.remove("hidden");
+}
+restartBtn.addEventListener("click", () => {
+  boxes.forEach(box => box.innerText = "");
+  turnX = true;
+  gameOver = false;
+  overlay.classList.add("hidden");
+});
